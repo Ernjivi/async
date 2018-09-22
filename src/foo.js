4dd4ea69ebad580c0c669bccd57f0e6c7282a7b6
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2018-09-22 10:57:48 -0500

    Finished babel configuration

--- src/foo.js
+++ src/foo.js
@@ -1,7 +1,5 @@
-class Foo {
+export default class Foo {
     constructor(){
         console.log("Hola mundo!");
     }
 }
-
-var foo = new Foo();
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
+    context: path.resolve(__dirname, 'src'),
     entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-    }
+    },
+    module: {
+        rules: [
+            {test: /\.js/, use: 'babel-loader'}
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
 }
